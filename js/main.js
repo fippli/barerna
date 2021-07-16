57062388fc132d7bd2f0f28adb1d3512301e4111
Filip Johansson <filip.johansson@protonmail.com>
2021-07-16 16:38:49 +0200

    feature: filter on tag

--- js/main.js
+++ js/main.js
@@ -1,114 +1,136 @@
-/**
- * Utils
- */
- const chain = (value, ...functions) => {
-  if (functions.length === 0) {
-    return value;
-  }
 
-  const [next, ...rest] = functions; 
-  return chain(next(value), ...rest);
-};
-
-const prepareArguments = (...params) => (func) => func(...params);
-
-/**
- * DOM manipulation
- */
-
-const createElement = (element) => document.createElement(element);
-const assignCssClass = (cssClass) => (element) => {
-  element.className = cssClass;
-  return element;
-};
-
-const appendChild = (child) => (element) => {
-  console.log(child, element)
-  element.appendChild(child);
-  return element;
-};
-
-const setText = (text) => (element) => {
-  element.appendChild(document.createTextNode(text));
-  return element;
-};
-
-const setLink = (link) => (element) => {
-  element.href = link;
-  element.appendChild(document.createTextNode(link));
-  return element;
-};
-
-const assignProps = (props) => element => {
-  Object.keys(props)
-  .forEach(prop => {
-    element[prop] = props[prop];
-  })
-  return element;
-}
-
-const createBarSubComponent = (elementType, className) => (data) => chain(
-    elementType,
-    createElement,
-    assignProps({className}),
-    setText(data)
-  );
-
-const createTitle = createBarSubComponent('h2', 'bar-title');
-const createAddress = createBarSubComponent('p', 'bar-address')
-const createTag = createBarSubComponent('div', 'tag')
-
-const createWebsite = ({website}) => chain(
-  'a',
-  createElement,
-  assignCssClass('bar-website'),
-  setLink(website)
-);
-
-const createMap = ({mapUrl}) => appendChild(
-  chain(
-    'iframe',
-    createElement,
-    assignProps({
-      src: mapUrl,
-      width: "600",
-      height: "450",
-      style: "border:0;",
-      allowfullscreen: "",
-      loading: "lazy",
-      className: "map"
-    })
-  )
-)(chain('div', createElement, assignProps({className: 'map-wrapper'})));
-
-const createTagList = ({tags}) => {
-  const tagList = chain('div', createElement, assignProps({className: 'tag-list-wrapper'}));
-  tags.map(createTag)
-    .map(appendChild)
-    .forEach(prepareArguments(tagList));
-  return tagList;
-}
-
-const constructBar = (bar) => chain(
-  'div', 
-  createElement, 
-  assignCssClass('bar-wrapper'),
-  appendChild(createTitle(bar.name)),
-  appendChild(createWebsite(bar)),
-  appendChild(createTagList(bar)),
-  appendChild(createMap(bar)),
-);
-
-
-/**
- * Main
- */
 
 (function () {
-  'use strict';
+  // 'use strict';
 
-  bars.map(constructBar)
-    .map(appendChild)
-    .forEach(prepareArguments(document.getElementById('bar-feed')));
+  const notImplemented = (_) => { /* Do nothing */ };
+
+  const chain = (value, ...functions) => {
+    if (functions.length === 0) {
+      return value;
+    }
+  
+    const [next, ...rest] = functions; 
+    return chain(next(value), ...rest);
+  };
+  
+  const prepareArguments = (...params) => (func) => func(...params);
+  
+  const createElement = (element) => document.createElement(element);
+  const assignCssClass = (cssClass) => (element) => {
+    element.className = cssClass;
+    return element;
+  };
+  
+  const appendChild = (child) => (element) => {
+    element.appendChild(child);
+    return element;
+  };
+  
+  const setText = (text) => (element) => {
+    element.appendChild(document.createTextNode(text));
+    return element;
+  };
+  
+  const setLink = (link) => (element) => {
+    element.href = link;
+    element.appendChild(document.createTextNode(link));
+    return element;
+  };
+  
+  const assignProps = (props) => element => {
+    Object.keys(props)
+    .forEach(prop => {
+      element[prop] = props[prop];
+    })
+    return element;
+  };
+
+  const setOnClick = (func) => (element) => {
+    element.onclick = func;
+    return element;
+  };
+
+  const createBarSubComponent = (elementType, className, onClick = notImplemented) => (data) => chain(
+      elementType,
+      createElement,
+      assignProps({className}),
+      setText(data),
+      setOnClick(onClick)
+    );
+  
+  const createTitle = createBarSubComponent('h2', 'bar-title');
+  const createTag = createBarSubComponent('button', 'tag', (event) => { updateState({filter: event.target.firstChild.data}) })
+  
+  const createWebsite = ({website}) => chain(
+    'a',
+    createElement,
+    assignCssClass('bar-website'),
+    setLink(website)
+  );
+  
+  const createMap = ({mapUrl}) => appendChild(
+    chain(
+      'iframe',
+      createElement,
+      assignProps({
+        src: mapUrl,
+        width: "600",
+        height: "450",
+        style: "border:0;",
+        allowfullscreen: "",
+        loading: "lazy",
+        className: "map"
+      })
+    )
+  )(chain('div', createElement, assignProps({className: 'map-wrapper'})));
+  
+  const createTagList = ({tags}) => {
+    const tagList = chain('div', createElement, assignProps({className: 'tag-list-wrapper'}));
+    tags.map(createTag)
+      .map(appendChild)
+      .forEach(prepareArguments(tagList));
+    return tagList;
+  }
+  
+  const constructBar = (bar) => chain(
+    'div', 
+    createElement, 
+    assignCssClass('bar-wrapper'),
+    appendChild(createTitle(bar.name)),
+    appendChild(createWebsite(bar)),
+    appendChild(createTagList(bar)),
+    appendChild(createMap(bar)),
+  );
+  
+  let state = {};
+
+  const updateState = (update) => {
+    state = {...state, ...update};
+    render();
+  }
+  
+  const barFilter = (bar) => {
+    if (state.filter) {
+      return bar.tags.includes(state.filter);
+    }
+    return true;
+  };
+
+  const clearFeed = () => {
+    const feed = document.getElementById('bar-feed');
+    feed.innerHTML = null;
+  }
+
+  const render = () => {
+    clearFeed();
+    bars.filter(barFilter)
+      .map(constructBar)
+      .map(appendChild)
+      .forEach(prepareArguments(document.getElementById('bar-feed')));
+  };
+
+  render();
+    
 })();
 
